b6fed489e9d9ef484aea66ff10a443f38e5a84d0
reganh342 <rehuggins14@gmail.com>
2021-12-06 17:11:38 -0500

    declared navx/ahrs

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -9,12 +9,14 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.kauailabs.navx.frc.AHRS;
 
 public class DriveSubsystem extends SubsystemBase {
 
   private WPI_TalonSRX leftMotor;
   private WPI_TalonSRX rightMotor;
   private DifferentialDrive m_drive;
+  private AHRS navX;
 
   /** Creates a new DriveSubsystem. */
   public DriveSubsystem() {
@@ -22,12 +24,21 @@ public class DriveSubsystem extends SubsystemBase {
     leftMotor = new WPI_TalonSRX(Constants.LEFTMOTOR);
     rightMotor = new WPI_TalonSRX(Constants.RIGHTMOTOR);
     m_drive = new DifferentialDrive(leftMotor, rightMotor);
+    navX = new AHRS();
 
   }
 
   public void drive(double leftSpeed, double rightSpeed){
 
     m_drive.tankDrive(leftSpeed, rightSpeed);
+  }
+
+  public double getAngle() {
+    return navX.getAngle();
+  }
+
+  public void printAngle() {
+    System.out.println(getAngle());
   }
 
   @Override
